1f0bfd661f049e184840bbe822735d68e2c038a3
Leand 3000 <theleand3000@gmail.com>
2021-01-17 00:59:38 +0000

    Function hexify created.

--- main.clj
+++ main.clj
@@ -1,1 +1,14 @@
-(println "Hi")
+(defn hexify "Convert byte sequence to hex string" [coll]
+  (let [hex [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \a \b \c \d \e \f]]
+      (letfn [(hexify-byte [b]
+        (let [v (bit-and b 0xFF)]
+          [(hex (bit-shift-right v 4)) (hex (bit-and v 0x0F))]))]
+        (apply str (mapcat hexify-byte coll)))))
+
+;; (defn hexify-str [s]
+;;   (hexify (.getBytes s))
+
+(hexify[11])
+
+
+;; (defn test ((is (= "65" (hexify[101])))))
